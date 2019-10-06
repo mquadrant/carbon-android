6f24f9e8edd2d9cac62de8b24b8d29b91b577c3f
Benjamin Mark <mquadrant@gmail.com>
2019-10-06 20:57:13 +0100

    Added a sample test

---
+++ src/test/java/Tests.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class Tests extends BaseClass {
+
+    @Test
+    public void sampleTest(){
+        System.out.println("I am inside sample test");
+    }
+}
