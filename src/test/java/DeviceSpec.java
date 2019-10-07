c426f192534bb03e86367f381e00d4ada8dc0b17
Benjamin Mark <mquadrant@gmail.com>
2019-10-07 14:39:49 +0100

    create class for device constants

---
+++ src/test/java/DeviceSpec.java
@@ -0,0 +1,11 @@
+public class DeviceSpec {
+
+    public static String DEVICE_NAME = "Nexus_5_API_24";
+    public static String UDID = "emulator-5554";
+    public static String PLATFORM_NAME = "ANDROID";
+    public static String PLATFORM_VERSION = "6.0";
+
+    public static String APP_PACKAGE = "com.lenddo.mobile.paylater.staging";
+    public static String APP_ENTRY_POINT = "com.lenddo.mobile.paylater.home.activity.HomeActivity";
+
+}
