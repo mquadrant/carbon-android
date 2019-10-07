5625a20e282715b5ebca50f496014eb3305ec4ae
Benjamin Mark <mquadrant@gmail.com>
2019-10-07 01:30:03 +0100

    added test case for sign in

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,11 +1,25 @@
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
+import helpers.RandomString;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import org.apache.http.util.Asserts;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static org.testng.FileAssert.fail;
+
 public class Tests extends BaseClass {
+
+    private String loginPhoneNumber = "08990001099";
+    private String loginPin = "1234";
+    private String wrongPhoneNumber = "080"+new RandomString(9).getString(8);
+    private String wrongPin = new RandomString(9).getString(4);
+    private String verificationCode = "123456";
 
     @Test
     public void sampleTest(){
@@ -15,17 +29,17 @@ public class Tests extends BaseClass {
     @Test
     public void navigateToSignInTest(){
         // creates a toggle for the given test, adds all log events under it
-        ExtentTest testSignIn = extent.createTest("Welcome Page", "Test Scenario for Navigation to Sign In");
+        ExtentTest navigateSignIn = extent.createTest("Welcome Page", "Test Scenario for Navigation to Sign In");
 
         // log(Status, details)
-        testSignIn.log(Status.INFO, "Navigation Test Started");
+        navigateSignIn.log(Status.INFO, "Navigation Test Started");
 
         driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/user_type_existing")).click();
         try{
             Assert.assertEquals(driver.findElementById("com.lenddo.mobile.paylater.staging:id/sign_in_next").getText(),"Sign in");
-            testSignIn.log(Status.PASS, "Navigate to Login View");
+            navigateSignIn.log(Status.PASS, "Navigate to Login View");
         }catch(AssertionError ex){
-            testSignIn.log(Status.FAIL, "Cant Sign in on this view");
+            navigateSignIn.log(Status.FAIL, "Cant Sign in on this view");
         }
 
     }
@@ -38,7 +52,40 @@ public class Tests extends BaseClass {
         // log(Status, details)
         testSignIn.log(Status.INFO, "SignIn Test Started");
 
-        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/user_type_existing")).click();
-        testSignIn.log(Status.PASS, "Navigate to Login View");
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_next")).click();
+        try{
+            Assert.assertEquals(driver.findElementById("com.lenddo.mobile.paylater.staging:id/textinput_error").getText(),"Input your phone number");
+            testSignIn.log(Status.PASS, "form validate is implemented");
+        }catch(AssertionError ex){
+            testSignIn.log(Status.FAIL, "form validate is not implemented");
+        }
+
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_phone")).click();
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_phone")).sendKeys(wrongPhoneNumber);
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_pin")).click();
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_pin")).sendKeys(wrongPin);
+        if(driver.isKeyboardShown()){
+            driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+        }
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_next")).click();
+
+        try{
+            Assert.assertEquals(driver.findElementById("com.lenddo.mobile.paylater.staging:id/alertTitle").getText(),"Error");
+            testSignIn.log(Status.PASS, "Should not Login with incorrect credential");
+        }catch(AssertionError ex){
+            testSignIn.log(Status.FAIL, "Login with incorrect credential");
+        }
+        driver.findElement(By.id("android:id/button1")).click();
+
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_phone")).click();
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_phone")).sendKeys(loginPhoneNumber);
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_pin")).click();
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_pin")).sendKeys(loginPin);
+        if(driver.isKeyboardShown()){
+            driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+        }
+        driver.findElement(By.id("com.lenddo.mobile.paylater.staging:id/sign_in_next")).click();
+        testSignIn.log(Status.PASS, "Login to verification view");
     }
+
 }
